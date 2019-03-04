8cfe7c3bc89793231dfbefa4277580610ae24bae
nidunlove <nidunlove@163.com>
2019-03-04 11:49:52 +0800

    百度统计

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,10 +2,21 @@
   <footer class="footer">
     <div class="footer-con">
       <div>Copyright<span style="margin:0 3px">&copy;</span>2019<a href="http://www.668ni.com/" target="_blank">&nbsp;倪盾的个人空间</a>. All Rights Reserved.</div>
-      <div><a href="http://www.miitbeian.gov.cn/" target="_blank">苏ICP备19008760号</a></div>
+      <div><a href="http://www.miitbeian.gov.cn/" target="_blank">苏ICP备19008760号-1</a></div>
     </div>
   </footer>
 </template>
+<script>
+//百度统计
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?33ab7e26d50aacf7b2ea61652e7d0e2b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
 <script>
   export default {
   // components: {
