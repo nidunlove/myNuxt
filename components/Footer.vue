79808c35e73c74346f190505dce30533e88780ab
nidun <nidunlove@163.com>
2021-01-20 10:40:45 +0800

    备案链接

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,15 +1,15 @@
 <template>
   <footer class="footer">
     <div class="footer-con">
-      <div class="footer-item">Copyright<span style="margin:0 3px">&copy;</span>2019&nbsp;<a href="http://www.668ni.com/" target="_blank">倪盾的个人空间</a>. All Rights Reserved.</div>
+      <div class="footer-item">Copyright<span style="margin:0 3px">&copy;</span>2019&nbsp;<a href="http://www.668ni.com/" target="_blank">倪盾的个人博客</a>. All Rights Reserved.</div>
       <div class="footer-item">站长邮箱 <a href="mailto:nidunlove@163.com" target="_blank">nidunlove@163.com</a></div>
       <div class="footer-item"><nuxt-link :to="{name:'adminLogin'}" target="_blank">我的后台</nuxt-link></div>
-      <div class="footer-item"><a href="http://www.miitbeian.gov.cn/" target="_blank">苏ICP备19008760号-1</a></div>
+      <div class="footer-item"><a href="https://beian.miit.gov.cn" target="_blank">苏ICP备19008760号-1</a></div>
       <div class="footer-item">Powered by <a href="https://zh.nuxtjs.org/" target="_blank">Nuxt.Js</a></div>
     </div>
     <el-backtop target=""></el-backtop>
   </footer>
-  
+
 </template>
 
 
